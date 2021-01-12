fd437df8e23f0647f294ab6018b2f333392c52af
hessikaveh <hesam.kaveh@gmail.com>
2021-01-12 13:30:37 +0100

    'fix'

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const
   express = require('express'),
   serveStatic = require('serve-static'),
   history = require('connect-history-api-fallback'),
-  port = process.env.PORT || 5000
+  port = process.env.PORT || 8080
 
 const app = express()
 
 app.use(history())
-app.use(serveStatic(__dirname + 'web/quasar-teaandjudgement/dist/spa'))
+app.use(serveStatic(__dirname + '/web/quasar-teaandjudgment/dist/spa'))
 app.listen(port)
