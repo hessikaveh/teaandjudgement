01eb4a5a1283274515f2693bf9db92bb29f0f971
hessikaveh <hesam.kaveh@gmail.com>
2021-01-12 12:36:08 +0100

    'fix'

--- server.js
+++ server.js
@@ -7,5 +7,5 @@ const
 const app = express()
 
 app.use(history())
-app.use(serveStatic(__dirname + '/dist/spa'))
+app.use(serveStatic(__dirname + 'web/quasar-teaandjudgement/dist/spa'))
 app.listen(port)
