602149d5ceb67aefbce70ca7a6753f88f6f22478
hessikaveh <hesam.kaveh@gmail.com>
2021-01-15 18:21:36 +0100

    fix

--- run.py
+++ run.py
@@ -4,6 +4,7 @@ WSGI entry point
 import os
 from backend.main import app
 
-if not os.path.exists('./uploads'):
-    os.mkdir('./uploads')
-app.run()
+if __name__ == '__main__':
+    if not os.path.exists('./uploads'):
+        os.mkdir('./uploads')
+    app.run()
