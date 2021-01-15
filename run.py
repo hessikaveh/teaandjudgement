5a50b18fc26c14add0c13c0d98b2eeb683c8c972
hessikaveh <hesam.kaveh@gmail.com>
2021-01-15 18:27:38 +0100

    fix

--- run.py
+++ run.py
@@ -5,6 +5,6 @@ import os
 from backend.main import app
 
 if __name__ == '__main__':
-    if not os.path.exists('./uploads'):
-        os.mkdir('./uploads')
+    if not os.path.exists('backend/uploads'):
+        os.mkdir('backend/uploads')
     app.run()
